bab18e2da39bf1bf2664fdb45faa1e0fbb56c0e3
kellerkatlin <kellerkatlin.k@gmail.com>
2025-03-28 21:02:03 -0500

    thanks - codigo

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -56,7 +56,7 @@ export default function Contact() {
           <div className="xl:w-[54%] order-2 xl:order-none">
             <form
               className="flex flex-col gap-6 p-10 bg-[#d7d7dc] dark:bg-[#27272c] rounded-xl"
-              action="https://formsubmit.co/kellerkatlin.k@gmail.com"
+              action="https://formsubmit.co/d6afc79f9e1e278f0fcde22eb2d6b462 "
               method="POST"
             >
               <h3 className="text-4xl text-accent">
@@ -68,7 +68,13 @@ export default function Contact() {
               </p>
               {/*inputs */}
               <input type="hidden" name="_captcha" value="false" />
-                <input type="hidden" name="_template" value="table"/>
+              <input type="hidden" name="_template" value="table" />
+              <input
+                type="hidden"
+                name="_next"
+                value="https://kellerkatlin-dev.vercel.app/thanks"
+              />
+
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6 first-line:">
                 <Input
                   type="firstname"
