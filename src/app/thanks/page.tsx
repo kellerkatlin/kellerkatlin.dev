9c285f896b96e06c73edfe988710758f3d18a840
kellerkatlin <kellerkatlin.k@gmail.com>
2025-03-28 20:27:17 -0500

    thanksl

--- src/app/thanks/page.tsx
+++ src/app/thanks/page.tsx
@@ -1,11 +1,16 @@
-<section className="py-0">
-  <div className="container mx-auto">
-    gracias por tu mensaje, me pondré en contacto contigo lo más pronto posible.
-  </div>
-  <button
-    className="bg-accent text-white px-4 py-2 rounded-md mt-4"
-    onClick={() => (window.location.href = "/")}
-  >
-    Volver al inicio
-  </button>
-</section>;
+export default function Thanks() {
+  return (
+    <section className="py-0">
+      <div className="container mx-auto">
+        gracias por tu mensaje, me pondré en contacto contigo lo más pronto
+        posible.
+      </div>
+      <button
+        className="bg-accent text-white px-4 py-2 rounded-md mt-4"
+        onClick={() => (window.location.href = "/")}
+      >
+        Volver al inicio
+      </button>
+    </section>
+  );
+}
